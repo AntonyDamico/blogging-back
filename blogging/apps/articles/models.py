7a4f55f40f92148eaf53d3b26e1dd930fc31d73f
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-06 01:05:14 +0200

    Creating slug on save

--- blogging/apps/articles/models.py
+++ blogging/apps/articles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 
 class Article(models.Model):
@@ -6,6 +8,14 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     image = models.ImageField(upload_to='images/', default='images/default.jpg')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            random_string = get_random_string(length=10)
+            slug_string = f'{self.title} {random_string}'
+            self.slug = slugify(slug_string)
+
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
