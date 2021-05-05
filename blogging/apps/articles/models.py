812837c88d48bd6f03277cbf48f3cf9d059748de
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-06 01:04:14 +0200

    Creating basic article model

--- blogging/apps/articles/models.py
+++ blogging/apps/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    slug = models.SlugField(db_index=True, max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
+
+    def __str__(self):
+        return self.title
