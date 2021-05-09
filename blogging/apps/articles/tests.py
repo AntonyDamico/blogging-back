4c6c71d92adac89c1161dc601df97c00093a7370
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-09 19:25:01 +0200

    Adding setup method to tests

--- blogging/apps/articles/tests.py
+++ blogging/apps/articles/tests.py
@@ -6,16 +6,22 @@ from .models import Article
 
 class ArticleModelTest(TestCase):
 
+    def setUp(self):
+        self.set_author()
+        self.set_article()
+
+    def set_author(self):
+        author = User.objects.create(username="test_user", password="pass")
+        self.author = author
+
+    def set_article(self):
+        title = 'This is the article title'
+        body = 'This is the body of the article, it has more than 20 chars'
+        article = Article.objects.create(title=title, body=body, author=self.author)
+        self.article = article
+
     def test_adding_slug_to_article(self):
         """El artículo creado debe tenner un slug"""
-        author = User(username="test_user", password="pass")
-        author.save()
-
-        title = 'This is the article title'
-        article = Article(title=title, author=author)
-        article.save()
-
-        title_slug = slugify(title)
-        self.assertNotEqual(article.slug, '')
-        self.assert_(article.slug.startswith(title_slug))
-
+        title_slug = slugify(self.article.title)
+        self.assertNotEqual(self.article.slug, '')
+        self.assert_(self.article.slug.startswith(title_slug))
