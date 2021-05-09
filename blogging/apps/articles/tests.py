be2c01f80433ae2ad3e43fd6a71aae7300a89023
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-09 19:14:41 +0200

    Creating slug test

--- blogging/apps/articles/tests.py
+++ blogging/apps/articles/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+from .models import Article
 
-# Create your tests here.
+
+class ArticleModelTest(TestCase):
+
+    def test_adding_slug_to_article(self):
+        """El artículo creado debe tenner un slug"""
+        author = User(username="test_user", password="pass")
+        author.save()
+
+        title = 'This is the article title'
+        article = Article(title=title, author=author)
+        article.save()
+
+        title_slug = slugify(title)
+        self.assertNotEqual(article.slug, '')
+        self.assert_(article.slug.startswith(title_slug))
+
